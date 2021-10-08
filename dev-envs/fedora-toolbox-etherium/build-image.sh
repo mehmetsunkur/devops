1b8cfc75edd1b18b955a560b05f40cf4dc646593
Mehmet Sunkur <mehmetsunkur@gmail.com>
2021-10-08 14:14:24 +0100

    eth dev toolbox setup

---
+++ dev-envs/fedora-toolbox-etherium/build-image.sh
@@ -0,0 +1,1 @@
+podman build . -t msunkur-fedora-dev
---
+++ dev-envs/fedora-toolbox-etherium/create-container.sh
@@ -0,0 +1,4 @@
+podman kill msunkur-fedora-dev;
+podman rm msunkur-fedora-dev;
+toolbox create -c msunkur-fedora-dev -i localhost/msunkur-fedora-dev:latest;
+
