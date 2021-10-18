f40cdf70db5a81d162e24e0ea2ffbec698af4b5f
Mehmet Sunkur <mehmetsunkur@gmail.com>
2021-10-18 11:39:21 +0100

    keepassxc tool

--- dev-envs/local-fedora-silverblue-dev/setups.sh
+++ dev-envs/local-fedora-silverblue-dev/setups.sh
@@ -3,5 +3,4 @@ sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 
 # install tools at once
-sudo rpm-ostree install net-tools xrdp xorgxrdp code;
-
+sudo rpm-ostree install net-tools xrdp xorgxrdp code keepassxc;
