e203cf0e5b3ef030cf18a1229f2719e4daad5bc2
mehmetsunkur <mehmetsunkur@gmail.com>
2022-03-11 10:28:28 -0500

    Update setups.sh

--- dev-envs/local-fedora-silverblue-dev/setups.sh
+++ dev-envs/local-fedora-silverblue-dev/setups.sh
@@ -2,4 +2,6 @@ flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flat
 flatpak install app/com.visualstudio.code/x86_64/stable;
 
 # install tools at once
-sudo rpm-ostree install net-tools xrdp xorgxrdp code keepassxc;
+wget https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm
+sudo rpm-ostree install xrdp xorgxrdp google-chrome-stable_current_x86_64.rpm;
+
