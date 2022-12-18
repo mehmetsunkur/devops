dfacde9d4f1d892ca5dadcbfcb89d49b33f3a848
mehmetsunkur <mehmetsunkur@gmail.com>
2022-12-18 12:52:39 +0000

    game platforms setup

--- linux-setups/fedora37/setup.sh
+++ linux-setups/fedora37/setup.sh
@@ -5,5 +5,13 @@
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo;
 flatpak install -y app/com.visualstudio.code/x86_64/stable;
 
-# Install Chrome and XRDP
-sudo dnf install -y xrdp xorgxrdp https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm;
+
+# Add winehq repo for the latest version of wine
+sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/35/winehq.repo;
+
+# Install Chrome, XRDP, wine
+sudo dnf install -y xrdp xorgxrdp winehq-stable https://dl.google.com/linux/direct/google-chrome-stable_current_x86_64.rpm;
+
+# Install game platforms
+flatpak install -y net.lutris.Lutris;
+flatpak install -y flathub com.heroicgameslauncher.hgl;
