e67384e39eeb6b7a44c134edf3e4add0c1209e05
Mehmet Sunkur <mehmetsunkur@gmail.com>
2023-04-14 00:14:23 +0300

    libvirt updates

--- scripts/livirt/vm-attach-disk.sh
+++ scripts/livirt/vm-attach-disk.sh
@@ -5,9 +5,14 @@ sudo virsh attach-disk win10 /dev/disk/by-id/nvme-SAMSUNG_MZVLB2T0HMLB-00000_S4C
 sudo virsh attach-disk win10 /dev/disk/by-id/nvme-SAMSUNG_MZVLB2T0HMLB-00000_S4C2NY0N202137-part4 vdc --config --type disk
 sudo virsh attach-disk winc-no-bootefi /dev/disk/by-id/nvme-SAMSUNG_MZVLB2T0HMLB-00000_S4C2NY0N202137-part4 vdd --config --type sata --print-xml
 
+sudo virsh attach-disk winc /dev/disk/by-id/nvme-SAMSUNG_MZVLB2T0HMLB-00000_S4C2NY0N202137 vdc --config --type disk --targetbus sata
+sudo virsh attach-disk winc /dev/disk/by-id/nvme-SAMSUNG_MZVLB2T0HMLB-00000_S4C2NY0N202137 vdc --config --type disk --targetbus scsi
+
 
 # attaches a disk image file to a VM
 
 virsh attach-disk win10 /var/mnt/data/virt-images/winc-virt-diskwinc-disk2vhd-0.qcow2 vdp --subdriver qcow2
 virsh attach-disk win10 /var/mnt/data/virt-images/winc-virt-diskwinc-disk2vhd-1.qcow2 vdr --subdriver qcow2
 virsh attach-disk win10 /var/mnt/data/virt-images/winc-virt-diskwinc-disk2vhd-1.qcow2 vdr --subdriver qcow2
+
+
